8fbe12eb5b49b980d00f06668f590e5f7efb700e
Aseel Alasaad <aseelalasaad@gmail.com>
2021-08-29 19:35:33 +0300

    react project

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React  from "react";
+import Header from "./Component/Header";
+import Main from "./Component/Main";
+import Footer from "./Component/Footer";
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <>
+      
+      <Header />
+      <Main />
+      <Footer />
+      
+    </>
+    )
+  }
+}
+export default App;
