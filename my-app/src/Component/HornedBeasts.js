d2eebd33ae4efab62ec63644fe26f333481a3d84
Aseel Alasaad <aseelalasaad@gmail.com>
2021-08-30 17:27:04 +0300

    lab2

--- my-app/src/Component/HornedBeasts.js
+++ my-app/src/Component/HornedBeasts.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class HornedBeasts extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        this.state= {
+
+            vots:0
+        }
+
+    }
+
+       voteFavoritebeast = () =>{
+            this.setState({
+                vots : this.state.vots +1
+            })
+       } 
+    
     render()
     {
         return(
             <>
-                 <h2>{this.props.title}</h2>
+                 {/* <h2>{this.props.title}</h2>
                  <img src={this.props.imageUrl} alt=""></img>
-                 <p>{this.props.description}</p>
+                 <p>{this.props.description}</p> */}
+
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.imageUrl} onClick={this.voteFavoritebeast}/>
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                    ❤️= {this.state.vots}
+                    </Card.Text>
+                    <Card.Text>
+                    {this.props.description}
+                    </Card.Text>
+                   
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
 
             </>
         )
